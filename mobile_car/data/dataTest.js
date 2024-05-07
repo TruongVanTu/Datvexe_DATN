bc2a307948a49528ede13c54d81aa7e4b51a5439
Truong Tu <tutvph14566@fpt.edu.vn>
2024-05-07 21:38:27 +0700

    data

---
+++ mobile_car/data/dataTest.js
@@ -0,0 +1,510 @@
+export const Listroute = [
+  {
+    intendTime: 4,
+    departure: "Ben Tre",
+    destination: "TpHCM",
+    startTime: "07:00",
+    endTime: "05:00",
+    carType: "Xe Thuong",
+    price: 300000,
+    status: true,
+    licensePlates: "77B-4568",
+    promotions: {
+      _id: "63f397c6f1f99eff8a2a5411",
+      startDate: "2024-01-13T00:00:00Z",
+      endDate: "2024-03-13T00:00:00Z",
+      percentDiscount: null,
+      routeId: "63e12dc694ff2132f39477d5",
+
+      quantityTicket: null,
+      status: true,
+      title: "Giảm 150.000đ cho hóa đơn trên 1,000,000đ",
+      purchaseAmount: 1000000,
+      moneyReduced: 150000,
+      maximumDiscount: null,
+      budget: 100000,
+      promotionType: "63c252b102c477dd6927806e",
+    },
+  },
+  {
+    intendTime: 4,
+    departure: "Ben Tre",
+    destination: "TpHCM",
+    startTime: "08:00",
+    endTime: "05:00",
+    carType: "Limoshine",
+    price: 300000,
+    status: false,
+    licensePlates: "77B-4568",
+    promotions: {
+      _id: "63e23319dcd0e28736921454",
+      startDate: "2024-01-13T00:00:00Z",
+      endDate: "2024-03-13T00:00:00Z",
+
+      percentDiscount: 10,
+      routeId: "63e12dc694ff2132f39477d5",
+      quantityTicket: 2,
+      status: true,
+      title: "Giảm 10% cho hóa đơn trên 400,000đ",
+      purchaseAmount: null,
+      moneyReduced: null,
+      maximumDiscount: 100000,
+      budget: 990000,
+      promotionType: "63c252a602c477dd6927806c",
+    },
+  },
+  {
+    intendTime: 4,
+    departure: "Ben Tre",
+    destination: "TpHCM",
+    startTime: "09:00",
+    endTime: "05:00",
+    carType: "Limoshine",
+    price: 300000,
+    status: false,
+    licensePlates: "77B-4568",
+    promotions: {
+      _id: "63f397c6f1f99eff8a2a5411",
+      startDate: "2024-01-13T00:00:00Z",
+      endDate: "2024-03-13T00:00:00Z",
+      percentDiscount: null,
+      routeId: "63e12dc694ff2132f39477d5",
+
+      quantityTicket: null,
+      status: true,
+      title: "Giảm 150.000đ cho hóa đơn trên 1,000,000đ",
+      purchaseAmount: 1000000,
+      moneyReduced: 150000,
+      maximumDiscount: null,
+      budget: 1000000,
+      promotionType: "63c252b102c477dd6927806e",
+    },
+  },
+  {
+    intendTime: 4,
+    departure: "Ben Tre",
+    destination: "TpHCM",
+    startTime: "00:00",
+    endTime: "05:00",
+    carType: "Xe Thuong",
+    price: 200000,
+    status: true,
+    licensePlates: "77B-4568",
+    promotions: null,
+  },
+  {
+    intendTime: 4,
+    departure: "Ben Tre",
+    destination: "TpHCM",
+    startTime: "00:00",
+    endTime: "05:00",
+    carType: "Xe Thuong",
+    price: 200000,
+    status: false,
+    licensePlates: "77B-4568",
+  },
+];
+export const listChair = [
+  {
+    seats: "01",
+    status: false,
+
+    id: "63b281f4d571739a200sdac6e",
+  },
+  {
+    seats: "02",
+    status: false,
+
+    id: "63b281f4d571739a2sfac6e",
+  },
+  {
+    seats: "03",
+    status: false,
+
+    id: "63b281f4d571739a2fsfac6e",
+  },
+  {
+    seats: "04",
+    status: false,
+
+    id: "63b281f4d5717asa200fac6e",
+  },
+
+  {
+    seats: "05",
+    status: false,
+
+    id: "63b281f4d5717fsda200fac6e",
+  },
+  {
+    seats: "06",
+    status: false,
+
+    id: "63b281f4dasd1739a200fac6e",
+  },
+  {
+    seats: "07",
+    status: false,
+
+    id: "63b281f4d5fdf39a200fac6e",
+  },
+  {
+    seats: "08",
+    status: false,
+
+    id: "63b281f4d5717adf200fac6e",
+  },
+  {
+    seats: "09",
+    status: false,
+
+    id: "63b281f4d57fdsf9a200fac6e",
+  },
+  {
+    seats: "10",
+    status: false,
+
+    id: "63b281f4d5717sfdsd200fac6e",
+  },
+  {
+    seats: "11",
+    status: false,
+
+    id: "63b281f4d57173fsdf00fac6e",
+  },
+  {
+    seats: "12",
+    status: false,
+
+    id: "63b281f4d571739dsfs0fac6e",
+  },
+  {
+    seats: "13",
+    status: false,
+
+    id: "63b281f4d571739sfs0fac6e",
+  },
+  {
+    seats: "14",
+    status: false,
+
+    id: "63b281f4d5717sfsd200fac6e",
+  },
+  {
+    seats: "15",
+    status: true,
+
+    id: "63b281f4d571fsfsd200fac6e",
+  },
+  {
+    seats: "16",
+    status: true,
+
+    id: "63b281f4d57sfss9a200fac6e",
+  },
+  {
+    seats: "17",
+    status: true,
+
+    id: "63b281f4d57173acvsdf00fac6e",
+  },
+  {
+    seats: "18",
+    status: false,
+
+    id: "63b281f4d571739fasdff0fac6e",
+  },
+  {
+    seats: "19",
+    status: false,
+
+    id: "63b281f4d5717asdf200fac6e",
+  },
+  {
+    seats: "20",
+    status: false,
+
+    id: "63b281f4d57173ere00fac6e",
+  },
+  {
+    seats: "21",
+    status: true,
+
+    id: "63b281f4d5717fsda200fac6e",
+  },
+  {
+    seats: "22",
+    status: false,
+
+    id: "63b281f4d5717cdfaa200fac6e",
+  },
+  {
+    seats: "23",
+    status: false,
+
+    id: "63b281f4d5717asds200fac6e",
+  },
+  {
+    seats: "24",
+    status: true,
+
+    id: "63b281f4d57173asdfs00fac6e",
+  },
+];
+
+export const listChairLiMo = [
+  {
+    seats: "A-01",
+    status: false,
+    id: 1,
+  },
+  {
+    seats: "A-02",
+    status: false,
+    id: 2,
+  },
+  {
+    seats: "A-03",
+    status: false,
+    id: 3,
+  },
+  {
+    seats: "A-04",
+    status: false,
+    id: 4,
+  },
+  {
+    seats: "A-05",
+    status: false,
+    id: 5,
+  },
+  {
+    seats: "A-06",
+    status: false,
+    id: 6,
+  },
+  {
+    seats: "A-07",
+    status: false,
+    id: 7,
+  },
+  {
+    seats: "A-08",
+    status: false,
+    id: 15,
+  },
+  {
+    seats: "A-09",
+    status: false,
+    id: 124,
+  },
+  {
+    seats: "A-10",
+    status: false,
+    id: 1543,
+  },
+  {
+    seats: "A-11",
+    status: false,
+    id: 11231,
+  },
+  {
+    seats: "A-12",
+    status: true,
+    id: 14234,
+  },
+  {
+    seats: "A-13",
+    status: true,
+    id: 1756,
+  },
+  {
+    seats: "A-14",
+    status: false,
+    id: 121312,
+  },
+  {
+    seats: "A-15",
+    status: true,
+    id: 1756,
+  },
+  {
+    seats: "A-16",
+    status: false,
+    id: 186765,
+  },
+  {
+    seats: "A-17",
+    status: false,
+    id: 1967867,
+  },
+  {
+    seats: "A-18",
+    status: false,
+    id: 16745534,
+  },
+  {
+    seats: "B-02",
+    status: false,
+    id: 14233,
+  },
+  {
+    seats: "B-03",
+    status: false,
+    id: 121531,
+  },
+  {
+    seats: "B-04",
+    status: false,
+    id: 196345,
+  },
+  {
+    seats: "B-05",
+    status: false,
+    id: 112312,
+  },
+  {
+    seats: "B-06",
+    status: false,
+    id: 175467,
+  },
+  {
+    seats: "B-07",
+    status: false,
+    id: 164321,
+  },
+  {
+    seats: "B-08",
+    status: false,
+    id: 1745645,
+  },
+  {
+    seats: "B-09",
+    status: false,
+    id: 186756,
+  },
+  {
+    seats: "B-10",
+    status: true,
+    id: 17634534,
+  },
+  {
+    seats: "B-11",
+    status: false,
+    id: 131231,
+  },
+  {
+    seats: "B-12",
+    status: false,
+    id: 1978876,
+  },
+  {
+    seats: "B-13",
+    status: false,
+    id: 1123132,
+  },
+  {
+    seats: "B-14",
+    status: false,
+    id: 1412312,
+  },
+  {
+    seats: "B-15",
+    status: false,
+    id: 1785654,
+  },
+  {
+    seats: "B-16",
+    status: true,
+    id: 1123543,
+  },
+  {
+    seats: "B-17",
+    status: false,
+    id: 175463534,
+  },
+  {
+    seats: "B-18",
+    status: false,
+    id: 11231252,
+  },
+  {
+    seats: "B-01",
+    status: false,
+    id: 1213415,
+  },
+];
+
+export const ListTicket = [
+  {
+    intendTime: 4,
+    departure: "Ben Tre",
+    destination: "TpHCM",
+    startTime: "07:00",
+    endTime: "05:00",
+    carType: "Xe Thuong",
+    price: 200000,
+    chair: [
+      {
+        seats: "01",
+      },
+      {
+        seats: "02",
+      },
+    ],
+    date: "13-03-2024",
+    lastName: "Nguyen",
+    firstName: "A",
+    phone: "01123141",
+    busStation:
+      "Bến xe Miền Tây(số 395 Kinh Dương Vương, Phường An Lạc, Quận Bình Tân, TP.HCM)",
+  },
+
+  {
+    intendTime: 4,
+    departure: "Ben Tre",
+    destination: "TpHCM",
+    startTime: "02:00",
+    endTime: "05:00",
+    carType: "Xe Thuong",
+    price: 200000,
+    chair: [
+      {
+        seats: "01",
+      },
+      {
+        seats: "02",
+      },
+    ],
+    date: "13-03-2024",
+    lastName: "Nguyen",
+    firstName: "A",
+    phone: "01123141",
+    busStation:
+      "Bến xe Miền Tây(số 395 Kinh Dương Vương, Phường An Lạc, Quận Bình Tân, TP.HCM)",
+  },
+
+  {
+    intendTime: 4,
+    departure: "Ben Tre",
+    destination: "TpHCM",
+    startTime: "04:00",
+    endTime: "05:00",
+    carType: "Xe Thuong",
+    price: 200000,
+    chair: [
+      {
+        seats: "01",
+      },
+      {
+        seats: "02",
+      },
+    ],
+    date: "13-03-2024",
+    lastName: "Nguyen",
+    firstName: "A",
+    phone: "01123141",
+    busStation:
+      "Bến xe Miền Tây(số 395 Kinh Dương Vương, Phường An Lạc, Quận Bình Tân, TP.HCM)",
+  },
+];
